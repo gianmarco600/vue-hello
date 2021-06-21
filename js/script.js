d17b323dfbbc69b529e95ffe538ea0dd1c36cd2b
gianmarco <gianmarco.palombo600@gmail.com>
2021-06-21 16:41:18 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
+//primo metodo
+
+// const instance = new Vue(
+//     {
+//         el: '#app',
+//         data: {
+//             text:'titolo'
+//         }
+//     }
+// );
+
+// const img = new Vue(
+//     {
+//         el: '#img',
+//         data: {
+//             value: 'https://citynews-romatoday.stgy.ovh/~media/original-hi/602411987578/colosseo-roma-3.jpg'
+//         }
+//     }
+// )
+
+
+//secondo metodo
+
 const instance = new Vue(
     {
-        el :'#app'
+        el: '#app1',
+        data: {
+            messaggio:'titolo',
+            value: 'https://citynews-romatoday.stgy.ovh/~media/original-hi/602411987578/colosseo-roma-3.jpg'
+        }
     }
-)
+);
